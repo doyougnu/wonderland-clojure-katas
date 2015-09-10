5fa85597d5f4a5eeadf9696bfea90bfd116da239
doyougnu <jmy6342@gmail.com>
2015-09-09 21:29:48 -0500

    changes

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -6,6 +6,9 @@
         ret (drop n alphabet)]
     (flatten (conj sub ret))))
 
+(defn enumerateCipher []
+  (map (fn [a b] (sorted-map a (rotateAlpha b))) "abcdefghijklmnopqrstuvwxyz" (range 26)))
+
 (defn encode [keyword message]
   "ssfeme")
 
