cad3de26f4a9c2ae904e97f0203c1cc0c06f3f4e
doyougnu <jmy6342@gmail.com>
2015-09-13 22:37:13 -0500

    Finished helper functions

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -6,17 +6,27 @@
         ret (drop n alphabet)]
     (flatten (conj sub ret))))
 
-<<<<<<< HEAD
 (defn enumerateCipher []
-  (map (fn [a b] (sorted-map a (rotateAlpha b))) "abcdefghijklmnopqrstuvwxyz" (range 26)))
-=======
->>>>>>> de7f3ab33a0e6488938516bd56c532a2b059c47f
+  (clojure.walk/keywordize-keys (into {} (map (fn [a b] (sorted-map (str a) (rotateAlpha b))) "abcdefghijklmnopqrstuvwxyz" (range 26)))))
+
+(defn alphaMap []
+  (clojure.walk/keywordize-keys (into {} (map (fn [a b] (sorted-map (str a) b)) "abcdefghijklmnopqrstuvwxyz" (range)))))
+
+(defn encodeHelper [keyword message]
+  (let [keyword (map identity keyword)
+        message (map identity message)
+        msgcnt (count message)
+        keycnt (count keyword)]
+    (cond
+      (> keycnt msgcnt) (take msgcnt (concat keyword))
+      (<= keycnt msgcnt) (take msgcnt (reduce concat (repeat msgcnt keyword))))))
 
 (defn encode [keyword message]
-  "ssfeme")
+  (let [nk (encodeHelper keyword message)
+        ]))
 
 (defn decode [keyword message]
-  "decodeme")
+    "decodeme")
 
 (defn decypher [cypher message]
   "drme")
