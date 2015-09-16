29331092174f5595db39548b12b2a7ab744483cf
doyougnu <jmy6342@gmail.com>
2015-09-16 00:01:26 -0500

    Finished decode

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -26,13 +26,17 @@
 
 (defn encode [keyword message]
   (let [keyword  (strToKey (encodeHelper keyword message))
-        message (strToKey (map identity message))
+        message (strToKey message)
         ciphertable (enumerateCipher)
         alphamap (alphaMap)]
     (reduce str (map (fn [k m] (nth  (ciphertable m) (alphamap k))) keyword message))))
 
-(defn decode [keyword message]
-    "decodeme")
+(defn decode [keyword enmessage]
+  (let [keyword (strToKey (encodeHelper keyword enmessage))
+        enmessage (strToKey enmessage)
+        ciphertable (enumerateCipher)
+        alphamap (alphaMap)]
+    (reduce str (mapcat name (map (fn [kw enm] (apply first (filter #(= enm (nth (strToKey (last %)) (alphamap kw))) ciphertable))) keyword enmessage)))))
 
 (defn decypher [cypher message]
   "drme")
