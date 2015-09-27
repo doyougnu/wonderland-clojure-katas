f0974030675c3e62437569185e8ce68a209622a5
doyougnu <jmy6342@gmail.com>
2015-09-27 18:34:14 -0500

    Finished controller functions

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -2,5 +2,22 @@
 
 (def start-pos [[[:fox :goose :corn :you] [:boat] []]])
 
+(defn inVector? [coll key]
+  (some (partial = key) coll))
+
+(defn removeElem [coll elem]
+  (let [elemindex (.indexOf coll elem)
+        coll1 (subvec coll 0 elemindex)
+        coll2 (subvec coll (+ elemindex 1) (count coll))]
+    (into [] (flatten (conj coll1 coll2)))))
+
+(defn sendItem [state item]
+  ;;given start-pos and item returns vector of similar form as start-pos with item moved to
+  ;;other "shore"
+  (let [[[shore boat dest]] state]
+    (if (inVector? shore item)
+      [[(removeElem shore item)] [boat] [(conj dest item)]]
+     state)))
+
 (defn river-crossing-plan []
   start-pos)
