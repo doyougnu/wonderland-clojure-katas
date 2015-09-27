8aedabf8314683689f6404ae93edde3228513c40
doyougnu <jmy6342@gmail.com>
2015-09-27 11:09:53 -0500

    Finished wonderland-number, every? was key

--- wonderland-number/project.clj
+++ wonderland-number/project.clj
@@ -3,4 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :user {:plugins [[cider/cider-nrepl "0.9.0-SNAPSHOT"]
+                   [refactor-nrepl "0.3.0-SNAPSHOT"]]}
+
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [alembic "0.3.2"]
+                 [org.clojure/tools.nrepl "0.2.7"]
+                 [org.clojure/math.combinatorics "0.1.1"]])
--- wonderland-number/src/wonderland_number/finder.clj
+++ wonderland-number/src/wonderland_number/finder.clj
@@ -1,5 +1,24 @@
-(ns wonderland-number.finder)
+(ns wonderland-number.finder
+  (:require [clojure.math.combinatorics :as combo]))
+
+(defn toDigits
+  ([n] (toDigits (list) n))
+  ([acc n]
+   (if (= n 0)
+     acc
+     (recur (conj acc (mod n 10)) (quot n 10)))))
+
+(defn genMults [n]
+  (map (partial * n) [2 3 4 5 6]))
+
+(defn wonderland-number-helper
+  "takes a number, generates multiples of 2 3 4 5 6, then checks if the multiples list has the
+  same digits as the number, converting to sets with significant performance cost, a brute
+  force solution"
+  [n]
+  (let [mults (map #(into [] (sort %)) (map toDigits (genMults n)))
+        n (into [] (sort (toDigits n)))]
+    (every? (partial = n) mults)))
 
 (defn wonderland-number []
-  ;; calculate me
-  42)
+  (filter wonderland-number-helper (range 100000 180000)))
